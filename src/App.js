f84cef9b37410f6b0ff1b918b5ab17dd057f3a2c
igborre <iborreani@correo.um.edu.uy>
2024-09-05 12:34:29 -0300

    Pequeños arreglos

--- src/App.js
+++ src/App.js
@@ -5,41 +5,41 @@ import './App.css';
 function App() {
   return (
       <body className="App-body">
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <div className="App-buttons">
-              <button className="App-buttonSingUp">
-                <a
-                    href="https://youtube.com"
-                >
-                  Sing Up
-                </a>
-              </button>
-              <button className="App-buttonLogIn">
-                <a
-                    href="https://youtube.com"
-                >
-                  Log In
-                </a>
-              </button>
-            </div>
-          </header>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://youtube.com"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <div className="App-buttons">
+            <button className="App-buttonSingUp">
+              <a
+                  href="https://youtube.com"
+              >
+                Sing Up
+              </a>
+            </button>
+            <button className="App-buttonLogIn">
+              <a
+                  href="https://youtube.com"
+              >
+                Log In
+              </a>
+            </button>
+          </div>
+        </header>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+            className="App-link"
+            href="https://youtube.com"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </div>
       </body>
-)
-  ;
+  )
+      ;
 }
 
 export default App;
