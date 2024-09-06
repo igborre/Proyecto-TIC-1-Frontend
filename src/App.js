aa8a7dc8968d5f0e66f90b347e8e34536a25a6f8
igborre <iborreani@correo.um.edu.uy>
2024-09-06 10:41:40 -0300

    Implemtenacion de habilidad de cambio de paginas
    Log in lleva a una pagina nueva, y el logo a la pagina principal

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
+import React from 'react';
 import logo from './logo.jpeg';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import About from './Components/About';
 
 function App() {
   return (
-      <body className="App-body">
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <div className="App-buttons">
-            <button className="App-buttonSingUp">
-              <a
-                  href="https://youtube.com"
-              >
-                Sing Up
-              </a>
-            </button>
-            <button className="App-buttonLogIn">
-              <a
-                  href="https://youtube.com"
-              >
-                Log In
-              </a>
-            </button>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <Link to="./">
+              <img src={logo} className="App-logo" alt="logo"/>
+            </Link>
+            <div className="App-buttons">
+              <button className="App-buttonSingUp">
+                <a href="https://youtube.com">
+                  Sign Up
+                </a>
+              </button>
+              <button className="App-buttonLogIn">
+                <Link to="/about">
+                  Log In
+                </Link>
+              </button>
+            </div>
+          </header>
+
+          <div className="App-body">
+          <Routes>
+            <Route path="/about" element={<About />} />
+          </Routes>
           </div>
-        </header>
-      </div>
-      </body>
-  )
-      ;
+        </div>
+      </Router>
+  );
 }
 
 export default App;
---
+++ src/Components/About.js
@@ -0,0 +1,11 @@
+import '../App.css'
+
+function About() {
+    return (
+        <div className="App">
+            Hola
+        </div>
+    )
+    ;
+}
+export default About;
