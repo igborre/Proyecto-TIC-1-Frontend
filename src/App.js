ce83517ce9fdd7286115e43ff49327733308bac7
santi34mg <ssilvera@correo.um.edu.uy>
2024-09-07 18:01:01 -0300

    SignUp Form

--- src/App.js
+++ src/App.js
@@ -1,38 +1,36 @@
-import React from 'react';
-import logo from './logo.jpeg';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import LogIn from './Components/LogIn';
+import React from "react";
+import logo from "./logo.jpeg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import LogIn from "./Components/LogIn";
+import SignUp from "./Components/SignUp";
 
 function App() {
   return (
-      <Router>
-        <div className="App">
-          <header className="App-header">
-            <Link to="./">
-              <img src={logo} className="App-logo" alt="logo"/>
-            </Link>
-            <div className="App-buttons">
-              <button className="App-buttonSingUp">
-                <a href="https://youtube.com">
-                  Sign Up
-                </a>
-              </button>
-              <button className="App-buttonLogIn">
-                <Link to="/LogIn">
-                  Log In
-                </Link>
-              </button>
-            </div>
-          </header>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Link to="./">
+            <img src={logo} className="App-logo" alt="logo" />
+          </Link>
+          <div className="App-buttons">
+            <button className="App-buttonSingUp">
+              <Link to="/SignUp">Sign Up</Link>
+            </button>
+            <button className="App-buttonLogIn">
+              <Link to="/LogIn">Log In</Link>
+            </button>
+          </div>
+        </header>
 
-          <div className="App-body">
+        <div className="App-body">
           <Routes>
             <Route path="/LogIn" element={<LogIn />} />
+            <Route path="/SignUp" element={<SignUp />} />
           </Routes>
-          </div>
         </div>
-      </Router>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/SignUp.js
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+import "./SignUp.css";
+
+const SignUp = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const validate = () => {
+    const errors = {};
+
+    if (!formData.username.trim()) {
+      errors.username = "Name is required";
+    }
+    if (!formData.email.trim()) {
+      errors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      errors.email = "Email is invalid";
+    }
+    if (!formData.password) {
+      errors.password = "Password is required";
+    } else if (formData.password.length < 6) {
+      errors.password = "Password must be at least 6 characters";
+    }
+    if (formData.password !== formData.confirmPassword) {
+      errors.confirmPassword = "Passwords do not match";
+    }
+
+    return errors;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const validationErrors = validate();
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+    } else {
+      try {
+        console.log(JSON.stringify(formData));
+        // TODO: tener un endpoint de verdad
+        const response = await fetch("http://localhost:8081/auth/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          console.log("Login successful:", data);
+          // Handle success, like redirecting to another page
+        } else {
+          console.error("Error logging in:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+  };
+
+  return (
+    <div className="signup-container">
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit} className="signup-form">
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+          {errors.name && <span className="error">{errors.name}</span>}
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          {errors.email && <span className="error">{errors.email}</span>}
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+          {errors.password && <span className="error">{errors.password}</span>}
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+          />
+          {errors.confirmPassword && (
+            <span className="error">{errors.confirmPassword}</span>
+          )}
+        </div>
+
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
