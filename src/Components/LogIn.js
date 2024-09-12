657b21ea8d9309863516a7b66df174b3ee045628
Santiago-Silvera <santiagosil3434@gmail.com>
2024-09-12 12:23:05 -0300

    simpler login buttons

--- src/Components/LogIn.js
+++ src/Components/LogIn.js
@@ -5,34 +5,21 @@ import axios from "axios";
 const LogIn = ({ onLogIn }) => {
   axios.defaults.baseURL = "http://localhost:8080";
 
-  const handleGoogleLogin = async () => {
+  const handleGithubLogin = async () => {
     try {
       // Make a request to the backend server to initiate the Google OAuth2 flow
-      const response = await axios.get("/auth/google");
+      const response = await axios.get("/auth/github");
       window.location.href = response.data.redirectUrl;
     } catch (error) {
       console.error("Error initiating Google login:", error);
     }
   };
 
-  const handleFacebookLogin = async () => {
-    try {
-      // Make a request to the backend server to initiate the Facebook OAuth2 flow
-      const response = await axios.get("/auth/facebook");
-      window.location.href = response.data.redirectUrl;
-    } catch (error) {
-      console.error("Error initiating Facebook login:", error);
-    }
-  };
-
   return (
     <div>
       <h1>Titulo LogIn</h1>
-      <button onClick={handleGoogleLogin} className="LogIn-buttons">
+      <button onClick={handleGithubLogin} className="LogIn-buttons">
         Login with Google
-      </button>
-      <button onClick={handleFacebookLogin} className="LogIn-buttons">
-        Login with Facebook
       </button>
       <button onClick={onLogIn} className="LogIn-buttons">
         Hola
