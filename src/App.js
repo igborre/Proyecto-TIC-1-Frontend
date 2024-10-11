870559169715e75abc78e80d06f606e25fbe4a93
santi34mg <ssilvera@correo.um.edu.uy>
2024-10-10 23:52:40 -0300

    movie button and header improvements

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
-import React from 'react';
-import logo from "./logo.jpeg";
+import React from "react";
 import "./App.css";
-import { Route, Routes, Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import LogIn from "./Components/LogIn";
-import SignUp from "./Components/SignUp";
-import Auth from './Utils/Auth';
-import Reserva from './Components/Reserva'; // Asegúrate de tener el componente de Reserva
-import LoggedInHeader from './Components/LoggedInHeader';
-import LoggedOutHeader from './Components/LoggedOutHeader.js';
-
+import SignUp from "./Components/SignUp.js";
+import Auth from "./Utils/Auth";
+import Reserva from "./Components/Reserva"; // Asegúrate de tener el componente de Reserva
+import LoggedInHeader from "./Components/LoggedInHeader";
+import LoggedOutHeader from "./Components/LoggedOutHeader.js";
+import MovieList from "./Utils/MovieList.js";
 
 // Main App Component
 const App = () => {
+  const { isLoggedIn, handleLogin, handleLogout } = Auth();
 
-    const { isLoggedIn, handleLogin, handleLogout } = Auth();
+  return (
+    <div className="App">
+      <header className="App-header">
+        {isLoggedIn ? (
+          <LoggedInHeader handleLogout={handleLogout} />
+        ) : (
+          <LoggedOutHeader />
+        )}
+      </header>
 
-
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                {isLoggedIn ? (
-                    <LoggedInHeader handleLogout={handleLogout} />
-                ) : (
-                    <LoggedOutHeader />
-                )}
-            </header>
-
-            <div className="App-body">
-                <Routes>
-                    <Route path="/LogIn" element={<LogIn onLogIn={handleLogin} />} />
-                    <Route path="/SignUp" element={<SignUp />} />
-                    <Route path="/Reserva" element={<Reserva />} />
-                </Routes>
-            </div>
-        </div>
-    )
+      <div className="App-body">
+        <Routes>
+          <Route path="/LogIn" element={<LogIn onLogIn={handleLogin} />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/Reserva" element={<Reserva />} />
+          <Route path="/Movies" element={<MovieList />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default App;
-
--- src/Components/LoggedOutHeader.js
+++ src/Components/LoggedOutHeader.js
@@ -1,24 +1,28 @@
-
 import logo from "../logo.jpeg";
 import { Link } from "react-router-dom";
+import Button from "./Button";
+import "./Button.css";
 
 const LoggedOutHeader = () => (
-    <>
-        <Link to="/">
-            <img src={logo} className="App-logo" alt="logo" />
-        </Link>
-        <div className="App-buttons">
-            <button className="App-buttonSingUp">
-                <Link to="/SignUp">Sign Up</Link>
-            </button>
-            <button className="App-buttonLogIn">
-                <Link to="/LogIn">Log In</Link>
-            </button>
-            <button>
-                <Link to="/Reserva">reservas</Link>
-            </button>
-        </div>
-    </>
+  <>
+    <Link to="/">
+      <img src={logo} className="App-logo" alt="logo" />
+    </Link>
+    <div className="App-buttons">
+      <Link to="/SignUp" className="link-style">
+        <Button>Sign Up</Button>
+      </Link>
+      <Link to="/LogIn" className="link-style">
+        <Button>Log In</Button>
+      </Link>
+      <Link to="/Reserva" className="link-style">
+        <Button>Reservas</Button>
+      </Link>
+      <Link to="/Movies" className="link-style">
+        <Button>Movies</Button>
+      </Link>
+    </div>
+  </>
 );
 
 export default LoggedOutHeader;
